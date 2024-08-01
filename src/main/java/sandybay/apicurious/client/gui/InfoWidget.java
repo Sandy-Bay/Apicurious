2fd3d46c9db924c207cfdde608f93c02e0424770
Gigabit101 <thegigabit101@gmail.com>
2024-08-01 17:43:00 +0100

    More widget work

--- src/main/java/sandybay/apicurious/client/gui/InfoWidget.java
+++ src/main/java/sandybay/apicurious/client/gui/InfoWidget.java
@@ -25,6 +25,7 @@ public class InfoWidget extends AbstractWidget
     private final boolean openLeft;
     private List<Component> info;
 
+
     public InfoWidget(int pX, int pY, int pWidth, int pHeight, int openSizeWidth, int openSizeHeight, boolean openLeft, List<Component> info)
     {
         super(pX, pY, pWidth, pHeight, Component.empty());
@@ -44,32 +45,74 @@ public class InfoWidget extends AbstractWidget
         super.onClick(mouseX, mouseY, button);
     }
 
+    private static final float msPerUpdate = 16.667f;
+    private long lastUpdateTime = 0;
+
+    private void update()
+    {
+        long updateTime;
+        if (lastUpdateTime == 0) {
+            lastUpdateTime = System.currentTimeMillis();
+            updateTime = lastUpdateTime + Math.round(msPerUpdate);
+        } else {
+            updateTime = System.currentTimeMillis();
+        }
+
+        int moveAmount = (int) (8 * (updateTime - lastUpdateTime) / msPerUpdate);
+        lastUpdateTime = updateTime;
+
+        if(isOpen)
+        {
+            if (width < openSizeWidth)
+            {
+                width += moveAmount;
+            }
+
+            if(height < openSizeHeight)
+            {
+                height += moveAmount;
+            }
+        }
+
+        if(!isOpen)
+        {
+            if(width > closedSizeWidth)
+            {
+                width -= moveAmount;
+            }
+
+            if(height > closedSizeHeight)
+            {
+                height -= moveAmount;
+            }
+        }
+    }
+
+    private void renderBackground(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick)
+    {
+        update();
+        pGuiGraphics.blit(SCREEN_LOCATION, getX(), getY(), 0, 0, width, height);
+    }
+
     @Override
     protected void renderWidget(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick)
     {
         pGuiGraphics.setColor(1.0F, 0.0F, 0.0F, this.alpha);
         RenderSystem.enableBlend();
         RenderSystem.enableDepthTest();
-        if(!isOpen)
-        {
-            pGuiGraphics.blit(SCREEN_LOCATION, getX(), getY(), 0, 0, closedSizeWidth, closedSizeHeight);
-        } else
-        {
-            if(openLeft)
-            {
-                pGuiGraphics.blit(SCREEN_LOCATION, getX() - openSizeWidth + closedSizeWidth, getY(), 0, 0, openSizeWidth, openSizeHeight);
-            } else
-            {
-                pGuiGraphics.blit(SCREEN_LOCATION, getX(), getY(), 0, 0, openSizeWidth, openSizeHeight);
-                int xOffset = 22;
-                int yOffset = 8;
-                pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
 
-                for (Component component : info)
-                {
-                    pGuiGraphics.drawString(Minecraft.getInstance().font, component, getX() + xOffset, getY() + 5 + yOffset, -1, false);
-                    yOffset += 10;
-                }
+        renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+
+        if(isOpen)
+        {
+            int xOffset = 22;
+            int yOffset = 8;
+            pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+            for (Component component : info)
+            {
+                pGuiGraphics.drawString(Minecraft.getInstance().font, component, getX() + xOffset, getY() + 5 + yOffset, -1, false);
+                yOffset += 10;
             }
         }
         pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
